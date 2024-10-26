f278ece231469c4968bf117105eb34098cf41569
Velsimir <nintendocoin@yandex.ru>
2024-10-26 21:41:56 +0300

    Folder Interfaces was deleted

--- Assets/Homework6/Scripts/Interfaces/IMovable.cs
+++
@@ -1,7 +0,0 @@
-namespace DefaultNamespace
-{
-    public interface IMovable
-    {
-        public void MoveForward(float speed);
-    }
-}
--- Assets/Homework6/Scripts/Interfaces/IRotatable.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace DefaultNamespace
-{
-    public interface IRotatable
-    {
-        public void Rotate(Vector3 rotation, float speed);
-    }
-}
